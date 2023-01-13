002fd9d471d3ad1a3576ac3e444a905d3f8eb031
OSM Shwa <joshua.guzman.514@gmail.com>
2023-01-13 18:04:38 -0500

    changes

--- iterators.py
+++ iterators.py
@@ -1,8 +1,17 @@
 def main():
     countries = ("Germany", "France", "Italy", "Spain", "Portugal", "Greece")
-    
-    
+    country_iter = iter(countries)
+
+    # while True:
+    #     try:
+    #         country = next(country_iter)
+    #     except StopIteration:
+    #         break
+    #     else:
+    #         print(country)
+    for country in countries:
+        print(country)
+
+
 if __name__ == "__main__":
     main()
-    
-    
