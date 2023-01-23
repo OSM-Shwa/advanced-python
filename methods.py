c8d85f8bc7c8c91c3d8280c49994cd2c9c7ebede
OSM Shwa <joshua.guzman.514@gmail.com>
2023-01-22 22:11:48 -0500

    changes

---
+++ methods.py
@@ -0,0 +1,16 @@
+from typing import Self
+from datetime import date
+
+
+class Calculator:
+    def __init__(self, version: int):
+        self.version = version
+
+    def description(self):
+        print(f"Currently running Calculator on version: {self.version}")
+
+    def add_numbers()
+
+if __name__ == "__main__":
+    calc1 = Calculator(10)
+    calc2 = Calculator(200)
