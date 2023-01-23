4c44fe828cdca56a286a8dd11d3f71b3d77c7e5a
OSM Shwa <joshua.guzman.514@gmail.com>
2023-01-22 22:43:34 -0500

    changes

--- methods.py
+++ methods.py
@@ -2,6 +2,21 @@ from typing import Self
 from datetime import date
 
 
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        
+    def description(self) -> str:
+        return f'{self.name} is {self.age} years old.'
+    
+    @classmethod
+    def age_from_year(cls, name: str, birth_year: int) -> Self:
+        current_year: int = date.today().year
+        age: int = current_year - birth_year
+        return cls(name, age)
+        
+
 class Calculator:
     def __init__(self, version: int):
         self.version = version
@@ -15,9 +30,5 @@ class Calculator:
 
 
 if __name__ == "__main__":
-    calc1 = Calculator(10)
-    calc2 = Calculator(200)
-
-    calc1.description()
-    calc2.description()
-    
+    federico = Person.age_from_year("Federico", 1997)
+    print(federico.description())
